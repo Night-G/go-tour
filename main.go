557e9871ec02563d0e3ba0e6c15ff31bb562f716
Night-G <61281536+Night-G@users.noreply.github.com>
2020-02-21 11:22:51 +0200

    Задание

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	//"encoding/json"
+	"html/template"
+)
+func main() {
+
+
+	http.HandleFunc("/", mainPage)
+	port:=":9999"
+	println("Server listen on port:", port)
+	err := http.ListenAndServe(port, nil)
+	if err != 	nil {
+		log.Fatal("ListenAndServe", err)
+	}
+}
+
+type User struct {
+	FirstName string `json:"first_name"`
+	LastName string  `json:"last_name"`
+}
+
+
+func mainPage(w http.ResponseWriter, r *http.Request){
+	//user := User{"df","fg"}
+	//js, _ := json.Marshal(user)
+
+	tmpl, err:= template.ParseFiles("static/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	if err := tmpl.Execute(w, nil); err!= nil {
+		http.Error(w,err.Error(), 400)
+		return
+	}
+
+}
